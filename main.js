f97d90e529af3bfa35d7fa4664812969de1d7b88
ty96meh <hetalianoneko@gmail.com>
2023-08-19 18:47:27 +0100

    bar setting

--- main.js
+++ main.js
@@ -1,9 +1,70 @@
-window.onscroll = function() {scrollFunction()};
+document.addEventListener("DOMContentLoaded", function() {
+    resizeFunction();  
+    window.onscroll = function() {
+        scrollFunction()
+    };
+    window.onresize = function() {
+        resizeFunction()
+    };
+
+ 
+
+});
+
+
+function resizeFunction(){
+    var links = document.querySelectorAll(".nav-list a");
+  
+    for (var i = 0; i < links.length; i++) {
+      if (
+        window.outerWidth < 659 
+      ) {
+        links[i].classList.add("width-adjust");
+      } else {
+        links[i].classList.remove("width-adjust");
+      }
+    }
+}
+
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+
+// @media screen and (max-width: 523px) {
+//     .navbar-top {
+//         font-size: 15px;
+//         border-radius: 7.5px;
+//     }
+// }
+
+// .navbar-top a {
+//     font-size: 18px;
+//     display:block; <---- edit to change by window width
+// }
+
 
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementByClassName("navbar").style.padding = "30px 10px";
-  } else {
-    document.getElementByClassName("navbar").style.padding = "80px 10px";
-  }
+    var menus = document.querySelectorAll(".menu-container");
+
+    var links = document.querySelectorAll(".navbar-top a");
+  
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) { 
+      menus.forEach(function(menu) {
+        menu.style.padding = '0px';
+      });
+
+      for (var i = 0; i < links.length; i++) {
+        links[i].classList.add("scroll-down");
+      }
+
+    } else {
+      menus.forEach(function(menu) {
+        menu.style.padding = '6px 0px';
+      });
+
+      for (var i = 0; i < links.length; i++) {        
+        links[i].classList.remove("scroll-down");
+      }
+    }
 }
+
