71e8a62e78888d04cebf9a85f8c4c54ce268cdfa
ty96meh <hetalianoneko@gmail.com>
2023-09-28 10:35:04 +0100

    Adjust

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,81 +1,81 @@
-document.addEventListener("DOMContentLoaded", function() {
-    resizeFunction(); 
-    scrollFunction();
+// document.addEventListener("DOMContentLoaded", function() {
+//     resizeFunction(); 
+//     scrollFunction();
 
-    window.onscroll = function() {
-        scrollFunction()
-    };
-    window.onresize = function() {
-        resizeFunction()
-    };
+//     window.onscroll = function() {
+//         scrollFunction()
+//     };
+//     window.onresize = function() {
+//         resizeFunction()
+//     };
 
  
 
-});
+// });
 
 
-function resizeFunction(){
-    var menus = document.querySelectorAll(".menu-container");
-    var links = document.querySelectorAll(".nav-list a");
-    for (var i = 0; i < links.length; i++) {
-      if (window.outerWidth < 659) {
-        links[i].classList.add("width-adjust");
+// function resizeFunction(){
+//     var menus = document.querySelectorAll(".menu-container");
+//     var links = document.querySelectorAll(".nav-list a");
+//     for (var i = 0; i < links.length; i++) {
+//       if (window.outerWidth < 659) {
+//         links[i].classList.add("width-adjust");
         
-        menus.forEach(function(menu) {
-          menu.style.display = 'inline-block';
-        });
-      } else {
-        links[i].classList.remove("width-adjust");
-        links[i].classList.remove("toggleChange");
-        menus.forEach(function(menu) {
-          menu.style.display = 'none';
-          menu.classList.remove("change");
-        });
-      }
-    }
+//         menus.forEach(function(menu) {
+//           menu.style.display = 'inline-block';
+//         });
+//       } else {
+//         links[i].classList.remove("width-adjust");
+//         links[i].classList.remove("toggleChange");
+//         menus.forEach(function(menu) {
+//           menu.style.display = 'none';
+//           menu.classList.remove("change");
+//         });
+//       }
+//     }
 
-}
+// }
 
 
-function scrollFunction() {
-    var menus = document.querySelectorAll(".menu-container");
+// function scrollFunction() {
+//     var menus = document.querySelectorAll(".menu-container");
 
-    var links = document.querySelectorAll(".navbar-top a");
+//     var links = document.querySelectorAll(".navbar-top a");
   
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) { 
-      menus.forEach(function(menu) {
-        menu.style.padding = '0px';
-      });
+//     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) { 
+//       menus.forEach(function(menu) {
+//         menu.style.padding = '0px';
+//       });
 
-      for (var i = 0; i < links.length; i++) {
-        links[i].classList.add("scroll-down");
-      }
+//       for (var i = 0; i < links.length; i++) {
+//         links[i].classList.add("scroll-down");
+//       }
 
-    } else {
-      menus.forEach(function(menu) {
-        menu.style.padding = '6px 0px';
-      });
+//     } else {
+//       menus.forEach(function(menu) {
+//         menu.style.padding = '6px 0px';
+//       });
 
-      for (var i = 0; i < links.length; i++) {        
-        links[i].classList.remove("scroll-down");
-      }
-    }
-}
-
-
+//       for (var i = 0; i < links.length; i++) {        
+//         links[i].classList.remove("scroll-down");
+//       }
+//     }
+// }
 
 
-function toggleFunction(x) {
-  x.classList.toggle("change");
-  var links = document.querySelectorAll(".nav-list a");
-    if (x.classList.contains("change") ) {
-      for (var i = 0; i < links.length; i++) {
-        links[i].classList.add("toggleChange");
-      }
-    } else {
-      for (var i = 0; i < links.length; i++) {
-        links[i].classList.remove("toggleChange");
-      }
-    }
+
+
+// function toggleFunction(x) {
+//   x.classList.toggle("change");
+//   var links = document.querySelectorAll(".nav-list a");
+//     if (x.classList.contains("change") ) {
+//       for (var i = 0; i < links.length; i++) {
+//         links[i].classList.add("toggleChange");
+//       }
+//     } else {
+//       for (var i = 0; i < links.length; i++) {
+//         links[i].classList.remove("toggleChange");
+//       }
+//     }
     
-}
+// }
